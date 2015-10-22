335386a3c7084e47c4ff4d7d20740639f86152e9
Chris <ceskiv@gmail.com>
2015-10-21 18:46:50 -0600

    expo

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+.controller('LoginCtrl', function ($scope, LoginService, $ionicPopup, $state) {
     $scope.data = {};
     $scope.userSession = null;
 
-    $scope.login = function() {
-        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+    $scope.login = function () {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function (data) {
             $scope.userSession = $scope.data.username;
             $state.go('tab.dash');
-        }).error(function(data) {
+        }).error(function (data) {
             var alertPopup = $ionicPopup.alert({
                 title: 'Login failed!',
                 template: 'Please check your credentials!'
@@ -16,19 +16,23 @@ angular.module('starter.controllers', [])
         });
     }
 
-    $scope.register = function() {
+    $scope.register = function () {
         $state.go('register');
     }
 })
 
-.controller('DashCtrl', function($scope, LoginService) {
+.controller('DashCtrl', function ($scope, LoginService) {
     $scope.userSession = LoginService.getProfileInfo();
     $scope.userScore = LoginService.getUserScore();
 })
 
-    .controller('RecycleCtrl', function($scope, ScoresService, LoginService, $ionicPopup) { 
-    $scope.data = {points:0};
-    $scope.recycleBtn = function() {
+.controller('RecycleCtrl', function ($scope, ScoresService, LoginService, $ionicPopup) {
+    
+//    $cordovaBarcodeScanner
+    $scope.data = {
+        points: 0
+    };
+    $scope.recycleBtn = function () {
         $scope.userSession = LoginService.getProfileInfo();
         ScoresService.addPoints($scope.userSession, $scope.data.points);
         var alertPopup = $ionicPopup.alert({
@@ -36,19 +40,29 @@ angular.module('starter.controllers', [])
             template: Number.parseInt($scope.data.points) + ' points added.'
         });
     }
-    
+
+//    $scope.scanBarcode = function () {
+//        $cordovaBarcodeScanner.scan().then(function (imageData) {
+//            alert(imageData.text);
+//            console.log("Barcode Format -> " + imageData.format);
+//            console.log("Cancelled -> " + imageData.cancelled);
+//        }, function (error) {
+//            console.log("An error happened -> " + error);
+//        });
+//    };
+
 })
 
-.controller('RegisterCtrl', function($scope, $ionicPopup, $state, RegisterService) {
+.controller('RegisterCtrl', function ($scope, $ionicPopup, $state, RegisterService) {
     $scope.data = {};
-    $scope.registerClick = function() {
-        RegisterService.registerUser($scope.data.regUsername, $scope.data.regPassword).success(function(data) {
+    $scope.registerClick = function () {
+        RegisterService.registerUser($scope.data.regUsername, $scope.data.regPassword).success(function (data) {
             var alertPopup = $ionicPopup.alert({
                 title: 'Success!',
                 template: 'User registered sucessfully!'
             });
             $state.go('login');
-        }).error(function(data) {
+        }).error(function (data) {
             var alertPopup = $ionicPopup.alert({
                 title: 'Error adding new user!',
                 template: 'Please check your credentials!'
@@ -57,19 +71,22 @@ angular.module('starter.controllers', [])
     }
 })
 
-.controller('Top10Ctrl', function($scope, ScoresService) {
-    
-    $scope.init = function() {
+.controller('Top10Ctrl', function ($scope, ScoresService) {
+
+    $scope.init = function () {
         $scope.top10Scores = ScoresService.getTop10Scores();
         $scope.top10Names = ScoresService.getTop10Names();
-        $scope.top10 = [];    
-        for(var i = 0; i < $scope.top10Scores.length; i++) {
-            var record = {name:"",score:""};
+        $scope.top10 = [];
+        for (var i = 0; i < $scope.top10Scores.length; i++) {
+            var record = {
+                name: "",
+                score: ""
+            };
             record.name = $scope.top10Names[i].name;
             record.score = $scope.top10Scores[i].score;
             $scope.top10.push(record);
         }
         console.log($scope.top10);
     }
-    
+
 });
