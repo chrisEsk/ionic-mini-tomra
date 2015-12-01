fc3423db1061fa5f482d388cbfd778ce09432525
Chris Esquivel <ceskiv@gmail.com>
2015-12-01 00:09:49 -0600

    almost there

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function ($scope, LoginService, $ionicPopup, $state) {
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
     $scope.data = {};
     $scope.userSession = null;
 
-    $scope.login = function () {
-        LoginService.loginUser($scope.data.username, $scope.data.password).success(function (data) {
+    $scope.login = function() {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
             $scope.userSession = $scope.data.username;
             $state.go('tab.dash');
-        }).error(function (data) {
+        }).error(function(data) {
             var alertPopup = $ionicPopup.alert({
                 title: 'Login failed!',
                 template: 'Please check your credentials!'
@@ -16,21 +16,21 @@ angular.module('starter.controllers', [])
         });
     }
 
-    $scope.register = function () {
+    $scope.register = function() {
         $state.go('register');
     }
 })
 
-.controller('DashCtrl', function ($scope, LoginService) {
+.controller('DashCtrl', function($scope, LoginService) {
     $scope.userSession = LoginService.getProfileInfo();
     $scope.userScore = LoginService.getUserScore();
 })
 
-.controller('RecycleCtrl', function ($scope, $state, ScoresService, LoginService, $ionicPopup, $cordovaBarcodeScanner) {
+.controller('RecycleCtrl', function($scope, $state, ScoresService, LoginService, $ionicPopup, $cordovaBarcodeScanner) {
     $scope.data = {
         points: 0
     };
-    $scope.recycleBtn = function () {
+    $scope.recycleBtn = function() {
         $scope.userSession = LoginService.getProfileInfo();
         ScoresService.addPoints($scope.userSession, $scope.data.points);
         var alertPopup = $ionicPopup.alert({
@@ -40,28 +40,34 @@ angular.module('starter.controllers', [])
         $state.go('tab.dash');
     }
 
-    $scope.scanBarcode = function () {
-        $cordovaBarcodeScanner.scan().then(function (imageData) {
-            alert(imageData.text);
+    $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+            $scope.userSession = LoginService.getProfileInfo();
+            ScoresService.addPoints($scope.userSession, Number.parseInt(imageData.text);
+            var alertPopup = $ionicPopup.alert({
+                title: 'Points Added!',
+                template: Number.parseInt(imageData.text) + ' points added.'
+            });
+            $state.go('tab.dash');
             console.log("Barcode Format -> " + imageData.format);
             console.log("Cancelled -> " + imageData.cancelled);
-        }, function (error) {
+        }, function(error) {
             console.log("An error happened -> " + error);
         });
     };
 
 })
 
-.controller('RegisterCtrl', function ($scope, $ionicPopup, $state, RegisterService) {
+.controller('RegisterCtrl', function($scope, $ionicPopup, $state, RegisterService) {
     $scope.data = {};
-    $scope.registerClick = function () {
-        RegisterService.registerUser($scope.data.regUsername, $scope.data.regPassword).success(function (data) {
+    $scope.registerClick = function() {
+        RegisterService.registerUser($scope.data.regUsername, $scope.data.regPassword).success(function(data) {
             var alertPopup = $ionicPopup.alert({
                 title: 'Success!',
                 template: 'User registered sucessfully!'
             });
             $state.go('login');
-        }).error(function (data) {
+        }).error(function(data) {
             var alertPopup = $ionicPopup.alert({
                 title: 'Error adding new user!',
                 template: 'Please check your credentials!'
@@ -70,9 +76,9 @@ angular.module('starter.controllers', [])
     }
 })
 
-.controller('Top10Ctrl', function ($scope, ScoresService) {
+.controller('Top10Ctrl', function($scope, ScoresService) {
 
-    $scope.init = function () {
+    $scope.init = function() {
         $scope.top10Scores = ScoresService.getTop10Scores();
         $scope.top10Names = ScoresService.getTop10Names();
         $scope.top10 = [];
@@ -89,7 +95,7 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('PrizesCtrl', function ($scope, $ionicPopup, $state, PrizeService, LoginService) {
+.controller('PrizesCtrl', function($scope, $ionicPopup, $state, PrizeService, LoginService) {
     $scope.userScore = LoginService.getUserScore();
     $scope.userPrizes = PrizeService.getUserPrizes($scope.userScore.score);
 });
